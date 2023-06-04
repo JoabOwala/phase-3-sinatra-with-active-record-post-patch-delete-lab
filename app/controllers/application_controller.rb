15abdbe0086f81d763036b417c9624122c9c13ec
JoabOwala <owalajoab@gmail.com>
2023-06-04 22:34:31 +0300

    Assignment Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
@@ -10,17 +20,33 @@ class ApplicationController < Sinatra::Base
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
   end
-
+  
   get '/baked_goods/by_price' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
     baked_goods.to_json
   end
-
+  
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+  
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    if bakery.update(name: params[:name])
+      bakery.to_json
+    else
+      status 400
+      { error: "Failed to update bakery" }.to_json
+    end
+  end
+  
 
+  delete '/baked_goods/:id' do
+    baked = BakedGood.find(params[:id])
+    baked.destroy
+    status 204
+  end
 end
